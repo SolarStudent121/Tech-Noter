55527f4fa9743e8fe02308c75e01536adfb71f46
Solarstudent121 <abhinavshettyr100@outlook.com>
2022-09-30 20:45:40 +0530

    fixed error

--- en/sw.js
+++ en/sw.js
@@ -3,27 +3,10 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.3/workbox
 
 self.addEventListener('install', function (event) {
     const data = [
-        "https://github.com/SolarStudent121/Tech-Noter/blob/1a8bcd89abce1f39390501266ec36dae6059f4c9/en/bootstrap/bootstrap-icons-1.9.1",
-        "https://code.jquery.com/jquery-3.6.1.min.js",
+        "/",
         "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js",
         "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css",
-        "https://github.com/SolarStudent121/Tech-Noter/blob/1a8bcd89abce1f39390501266ec36dae6059f4c9/en/home/styles.css",
-        "https://github.com/SolarStudent121/Tech-Noter/blob/1a8bcd89abce1f39390501266ec36dae6059f4c9/en/home/index.html",
-        "https://github.com/SolarStudent121/Tech-Noter/blob/1a8bcd89abce1f39390501266ec36dae6059f4c9/en/home/home.js",
-        "https://github.com/SolarStudent121/Tech-Noter/blob/1a8bcd89abce1f39390501266ec36dae6059f4c9/en/editor/styles.css",
-        "https://github.com/SolarStudent121/Tech-Noter/blob/1a8bcd89abce1f39390501266ec36dae6059f4c9/en/editor/index.html",
-        "https://github.com/SolarStudent121/Tech-Noter/blob/1a8bcd89abce1f39390501266ec36dae6059f4c9/en/editor/styles.css",
-        "https://github.com/SolarStudent121/Tech-Noter/blob/1a8bcd89abce1f39390501266ec36dae6059f4c9/en/docs/styles.css",
-        "https://github.com/SolarStudent121/Tech-Noter/blob/1a8bcd89abce1f39390501266ec36dae6059f4c9/en/docs/screenshots/index.html",
-        "https://github.com/SolarStudent121/Tech-Noter/blob/1a8bcd89abce1f39390501266ec36dae6059f4c9/en/docs/index.html",
-        "https://github.com/SolarStudent121/Tech-Noter/blob/1a8bcd89abce1f39390501266ec36dae6059f4c9/en/docs/doc.js",
-        "https://github.com/SolarStudent121/Tech-Noter/blob/1a8bcd89abce1f39390501266ec36dae6059f4c9/en/bootstrap/bootstrap-icons-1.9.1/bootstrap-icons.css",
-        "https://github.com/SolarStudent121/Tech-Noter/blob/1a8bcd89abce1f39390501266ec36dae6059f4c9/en/docs/screenshots/Screenshot.png",
-        "https://github.com/SolarStudent121/Tech-Noter/blob/1a8bcd89abce1f39390501266ec36dae6059f4c9/en/docs/screenshots/icon.png",
-        "https://github.com/SolarStudent121/Tech-Noter/blob/1a8bcd89abce1f39390501266ec36dae6059f4c9/en/manifest.webmanifest",
-        "https://github.com/SolarStudent121/Tech-Noter/blob/1a8bcd89abce1f39390501266ec36dae6059f4c9/en/sw.js",
-        "https://github.com/SolarStudent121/Tech-Noter/blob/1a8bcd89abce1f39390501266ec36dae6059f4c9/en/icons/icon.png",
-        "https://github.com/SolarStudent121/Tech-Noter/blob/1a8bcd89abce1f39390501266ec36dae6059f4c9/en"
+        "https://https://code.jquery.com/jquery-3.6.1.min.js"
     ]
     event.waitUntil(
         caches.open('tech-noter-v1').then(function (cache) {
